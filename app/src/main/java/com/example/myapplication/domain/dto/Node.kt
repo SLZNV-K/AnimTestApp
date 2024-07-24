0f27320b1072449b23e2b46dde5fac14b868818b
Ekaterina Selezneva <edselezneva@scid.ru>
2024-07-24 16:27:54 +0300

    added new nodes

--- app/src/main/java/com/example/myapplication/domain/dto/Node.kt
+++ app/src/main/java/com/example/myapplication/domain/dto/Node.kt
@@ -3,5 +3,18 @@ package com.example.myapplication.domain.dto
 data class Node(
     val id: Int,
     val message: String,
-    var edges: Edges
+    var edges: Edges = Edges(),
+    val foreground: Int = 0,
+    val emilyAnim: Int = 0,
+    val characterType: CharacterType = CharacterType.VOICE_OVER
 )
+
+enum class CharacterType(private val displayName: String) {
+    PLAYER("Player"),
+    EMILY("Emily"),
+    VOICE_OVER("Voice over");
+
+    override fun toString(): String {
+        return displayName
+    }
+}
